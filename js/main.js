3eb227cf47414c0eaecb55e11d343943ede11b64
poohlikeshoney <one69jy@gmail.com>
2021-07-05 08:21:16 +0900

    JS : Navbar menu click

--- js/main.js
+++ js/main.js
@@ -9,4 +9,18 @@ document.addEventListener('scroll', () => {
   }else{
     navbar.classList.remove('navbar--dark');
   }
-})
+});
+
+// navbar메뉴 클릭했을 때 스크롤링
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  //id가 클릭이 될때만 출력
+  const link = target.dataset.link;
+  if(link == null){
+    return;
+  }
+  // console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({behavior:"smooth"});
+});
