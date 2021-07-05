ee0561947a99d61b7be7b397a9b058e5dc707c33
poohlikeshoney <one69jy@gmail.com>
2021-07-05 10:04:50 +0900

    JS : Experience tab menu

--- js/main.js
+++ js/main.js
@@ -57,3 +57,28 @@ function scrollIntoViews(selector){
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior:"smooth"});
 }
+
+// ✅ Experience tab버튼
+const tabList = document.querySelectorAll('.experience .experience__tab-btn li');
+const contents = document.querySelectorAll('.experience .experience__content .content_box')
+let activeCont = ''; // 현재 활성화 된 컨텐츠 (기본:#tab1 활성화)
+
+for(var i = 0; i < tabList.length; i++){
+  tabList[i].querySelector('.tab').addEventListener('click', function(e){
+    e.preventDefault();
+    for(var j = 0; j < tabList.length; j++){
+      // 나머지 버튼 클래스 제거
+      tabList[j].classList.remove('active');
+
+      // 나머지 컨텐츠 display:none 처리
+      contents[j].style.display = 'none';
+    }
+
+    // 버튼 관련 이벤트
+    this.parentNode.classList.add('active');
+
+    // 버튼 클릭시 컨텐츠 전환
+    activeCont = this.getAttribute('href');
+    document.querySelector(activeCont).style.display = 'block';
+  });
+}
