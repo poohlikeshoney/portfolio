df76a05edc4dd6787826ad15e37ec31dc73f22d9
poohlikeshoney <one69jy@gmail.com>
2021-07-05 10:03:16 +0900

    JS : Scroll up button

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// navbar 스크롤 되면 box-shadow 만들기
+// ✅ navbar 스크롤 되면 box-shadow 만들기
 const navbar = document.querySelector('.navbar__container');
 const navbarheight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,7 +11,7 @@ document.addEventListener('scroll', () => {
   }
 });
 
-// navbar메뉴 클릭했을 때 스크롤링
+// ✅ navbar메뉴 클릭했을 때 스크롤링
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
   const target = event.target;
@@ -23,24 +23,35 @@ navbarMenu.addEventListener('click', (event) => {
   scrollIntoViews(link);
 });
 
-// home의 contact버튼 클릭했을 때 스크롤링
+// ✅ home의 contact버튼 클릭했을 때 스크롤링
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
   scrollIntoViews('#contact')
 });
 
-// 스크롤시 home 투명하게 처리
+// ✅ 스크롤시 home 투명하게 처리
 const home = document.querySelector('#home');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-  home.style.opacity = 1 - window.scrollY / (homeHeight - 300); 
+  home.style.opacity = 1 - window.scrollY / (homeHeight - 200); 
   // 1 - 0 / 800 = 1 ==> opacity:1
   // 1 - 400 / 800 = 0.5 ==> opacity:0.5
   // 1 - 800 / 800 = 0 ==> opacity:0
 });
 
-
-
+// ✅ 스크롤시 scroll up 버튼 생성
+const scrollUpBtn = document.querySelector('.scrollup-btn')
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight/2){
+    scrollUpBtn.classList.add('visible');
+  } else{
+    scrollUpBtn.classList.remove('visible');
+  }
+});
+// ✅ scroll up 클릭 시 home으로 이동
+scrollUpBtn.addEventListener('click', () => {
+  scrollIntoViews('#home')
+});
 
 function scrollIntoViews(selector){
   const scrollTo = document.querySelector(selector);
