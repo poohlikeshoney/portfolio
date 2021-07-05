1076f9a0d66de06ea274ca6839b2d4295ef1c1bc
poohlikeshoney <one69jy@gmail.com>
2021-07-05 09:17:18 +0900

    JS : Home Opacity

--- js/main.js
+++ js/main.js
@@ -27,7 +27,20 @@ navbarMenu.addEventListener('click', (event) => {
 const homeContactBtn = document.querySelector('.home__contact');
 homeContactBtn.addEventListener('click', () => {
   scrollIntoViews('#contact')
-})
+});
+
+// 스크롤시 home 투명하게 처리
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / (homeHeight - 300); 
+  // 1 - 0 / 800 = 1 ==> opacity:1
+  // 1 - 400 / 800 = 0.5 ==> opacity:0.5
+  // 1 - 800 / 800 = 0 ==> opacity:0
+});
+
+
+
 
 function scrollIntoViews(selector){
   const scrollTo = document.querySelector(selector);
